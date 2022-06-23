d22a4bdf03addf7de84419c4cd2b59453cc4df76
maatros <kosin.piotr@gmail.com>
2022-06-23 11:53:04 +0200

    removed emopty state added force_destroy parameter

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "docker-project-bucket"
+  force_destroy = true
   # Enable versioning so we can see the full revision history of our state files
   versioning {
     enabled = true
