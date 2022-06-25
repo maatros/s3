9edcb02aa57e3d4dd23e6ffdc91b69e1cfcdb908
maatros <kosin.piotr@gmail.com>
2022-06-25 15:57:27 +0200

    added block public lines

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = var.region
 }
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
+  block_public_acls   = true
+  block_public_policy = true
   force_destroy = var.bucket_force_destroy
   versioning {
     enabled = var.bucket_versioning
@@ -16,7 +17,6 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
 }
-
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table_name
   billing_mode = var.dynamodb_table_billing_mode
